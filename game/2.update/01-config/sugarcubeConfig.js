e791a69774346abe3bde53f3d3db7ea50673aca0
jjang-dol <seri.min.com@gmail.com>
2026-09-09 14:21:16 +0900

    시작 페이지 번역

--- game/2.update/01-config/sugarcubeConfig.js
+++ game/2.update/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.12.8",
+	version: "0.5.12.10",
 	versionName: `"Abyssal" 에디션`,
 	sneaky: false,
 	socialMediaEnabled: true,
