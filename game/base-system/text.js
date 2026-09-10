8db46e85af70d46ecc86acd69e1a2b6ee6c96318
jjang-dol <seri.min.com@gmail.com>
2026-09-10 12:08:45 +0900

    이것저것

--- game/base-system/text.js
+++ game/base-system/text.js
@@ -857,12 +857,12 @@ statDisplay.create("gworldcorruptionhard", () =>
 	statDisplay.statChange("세계에 새 구멍이 생긴다...", 0, "tentacle", () => V.temple_confessor_intro || V.gwylanSeen?.includes("ritual_beast"))
 );
 
-statDisplay.create("lheat", () => statDisplay.statChange("Heat", -1, "green"));
-statDisplay.create("llheat", () => statDisplay.statChange("Heat", -2, "green"));
-statDisplay.create("lllheat", () => statDisplay.statChange("Heat", -3, "green"));
-statDisplay.create("gheat", () => statDisplay.statChange("Heat", 1, "red"));
-statDisplay.create("ggheat", () => statDisplay.statChange("Heat", 2, "red"));
-statDisplay.create("gggheat", () => statDisplay.statChange("Heat", 3, "red"));
+statDisplay.create("lheat", () => statDisplay.statChange("더위", -1, "green"));
+statDisplay.create("llheat", () => statDisplay.statChange("더위", -2, "green"));
+statDisplay.create("lllheat", () => statDisplay.statChange("더위", -3, "green"));
+statDisplay.create("gheat", () => statDisplay.statChange("더위", 1, "red"));
+statDisplay.create("ggheat", () => statDisplay.statChange("더위", 2, "red"));
+statDisplay.create("gggheat", () => statDisplay.statChange("더위", 3, "red"));
 
 // These rely on the 'statChange' function in 'stat-changes.js'
 statDisplay.create("gharmony", (amount = 1) => statChange.harmony(amount));
